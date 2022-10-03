9dc7139f352deade60d2da1ffe5e59479ee91550
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-03 23:49:48 +0300

    lesson1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 25;
+        int myAge = 26;
+        long artemAge = 3000L;
+        float drob = 4.5f;
+        double drob2 = 5.555555555d;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char sym = ' ';
+        String line = "aleksey";
+        String surName = "popov";
+        System.out.println(line + " "+ surName);
+    }
+}
