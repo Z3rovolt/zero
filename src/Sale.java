5bad7a86ac00b09797d52f46adf1c53e0e8626d2
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-08 12:28:24 +0300

    lesson2

---
+++ src/Sale.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class Sale {
+    static Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double n , a , b, x , y;
+        System.out.println("Введите исходную стоимость товара");
+        n = SCANNER.nextDouble();
+        System.out.println("введите альтернативную стоимость товара");
+        a = SCANNER.nextDouble();
+        System.out.println("выберите скидку в процентах");
+        y = SCANNER.nextDouble();
+        System.out.println();
+        System.out.println("введите альтернативну стоимость2 товара");
+        b = SCANNER.nextDouble();
+        System.out.println("введите скидку2 в процентах");
+        x = SCANNER.nextDouble();
+        if (n<a && n<b){
+            System.out.println(n);
+        }
+        else if(n>a){
+            System.out.println((n*y)/100);
+        }
+        else if(n>b){
+            System.out.println((n*x)/100);
+        }
+    }
+
+}
