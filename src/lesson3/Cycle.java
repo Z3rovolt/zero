885f697859745752d1e1d7d2c0ab4d5ecd73149d
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-11 00:10:03 +0300

    lesson3

---
+++ src/lesson3/Cycle.java
@@ -0,0 +1,17 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Cycle {
+    static Scanner SCANNER = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100 ; i++){
+            if(i % 2 !=0){
+            System.out.println(i);
+        }}
+
+
+    }
+}
