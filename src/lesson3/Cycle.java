3f679aa5b6f2191ba3b0f558537ead2eb321189f
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-11 11:57:23 +0300

    lesson3

--- src/lesson3/Cycle.java
+++ src/lesson3/Cycle.java
@@ -3,12 +3,12 @@ package lesson3;
 public class Cycle {
 
 
-
     public static void main(String[] args) {
-        for(int i = 0; i < 100 ; i++){
-            if(i % 2 !=0){
-            System.out.println(i);
-        }}
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
 
 
     }
--- src/lesson3/Factorial.java
+++ src/lesson3/Factorial.java
@@ -1,9 +1,21 @@
 package lesson3;
 
+import java.util.Scanner;
+
 public class Factorial {
+    static Scanner SCANNER = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int n;
+        int result = 1;
+        System.out.println("введите число");
+        n = SCANNER.nextInt();
 
+
+        for (int i = 2; i <= n; i++) {
+           result *= i;
+        }
+        System.out.println(result);
 
 
     }
