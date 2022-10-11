af823d09c54ebc62461bc1851641235336f2df08
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-11 12:11:20 +0300

    lesson3

---
+++ src/lesson3/CycleWhile.java
@@ -0,0 +1,19 @@
+package lesson3;
+
+public class CycleWhile {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 100) {
+            i++;
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+
+
+        }
+
+
+    }
+}
