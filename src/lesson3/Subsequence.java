df1bd989dba34271dbeed7ee7776c6aa64e25110
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-12 12:14:38 +0300

    lesson3

--- src/lesson3/Subsequence.java
+++ src/lesson3/Subsequence.java
@@ -4,12 +4,11 @@ public class Subsequence {
 
     public static void main(String[] args) {
 
-        int result = 5;
 
+        for (int i = 0; i >= -45; i--) {
+            if(i % 5 ==0)
 
-        for (int i = 0; i < 10; i++) {
-            result -= 5 ;
-            System.out.println(result);
+            System.out.println(i);
 
 
         }
