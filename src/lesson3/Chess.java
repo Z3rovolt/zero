9a1bcf9e596b6551fa9388a0ff35fe2843684520
Z3rovolt <andrei.vasilenko16@gmail.com>
2022-10-14 20:28:35 +0300

    lesson3

---
+++ src/lesson3/Chess.java
@@ -0,0 +1,28 @@
+package lesson3;
+
+public class Chess {
+
+    public static void main(String[] args) {
+
+        String[][] array = new String[8][8];
+
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array.length; j++) {
+                if ((i + j) % 2 == 0) array[i][j] = "W";
+                else array[i][j] = "B";
+                System.out.print(" " + array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+}
+
+
+
+
